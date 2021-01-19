c09609939c9a49f1a4280ea5f728650dcd705421
Tiberius Colina <bluedragon852@icloud.com>
2021-01-18 21:15:28 -0500

    fixed DriveTrainCommand and DriveTrainReversedCommand (push 2)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -70,6 +70,7 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
+
     // Configure the button bindings
     configureButtonBindings();
     driveTrainSubsystem.setDefaultCommand(driveWithJoysticksCommand);
